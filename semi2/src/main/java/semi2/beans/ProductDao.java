5f45ee8a2ef8c268d9f3e5d8cac66642590ff810
syso950729 <syso950729@gmail.com>
2022-05-03 16:16:54 +0900

    풀리퀘스트 오류

--- semi2/src/main/java/semi2/beans/ProductDao.java
+++ semi2/src/main/java/semi2/beans/ProductDao.java
@@ -1,18 +1,6 @@
 package semi2.beans;
 
 public class ProductDao {
-<<<<<<< HEAD
 
 	//테스트
-=======
-	//test
-	//khacademy
-	//테스트1
-	//테스트2
-	//testvsss
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/main
-=======
-	//teet
->>>>>>> refs/remotes/origin/main
 }
