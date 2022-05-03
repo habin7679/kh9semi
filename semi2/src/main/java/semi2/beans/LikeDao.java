7083d908eaa58b71a1c994f9f96febf823570162
Alwayshyuk <never5686@naver.com>
2022-05-03 15:28:14 +0900

    코드정리(강사)

--- semi2/src/main/java/semi2/beans/LikeDao.java
+++ semi2/src/main/java/semi2/beans/LikeDao.java
@@ -5,17 +5,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 public class LikeDao {
-	public void insert(LikeDto likeDto) throws Exception{
-		
-		Connection con = JdbcUtils.getConnection();
-		
-		String sql = "insert into like values(?,?)";
-		
-		PreparedStatement ps = con.prepareStatement(sql);
-		
-		ps.setString(1, likeDto.getMemberId());
-		ps.setInt(2, likeDto.getProductNo());
-		
-		ps.execute();
-		con.close();		
-	}
+//	public void insert(LikeDto likeDto) throws Exception{
+//		
+//		Connection con = JdbcUtils.getConnection();
+//		
+//		String sql = "insert into like values(?,?)";
+//		
+//		PreparedStatement ps = con.prepareStatement(sql);
+//		
+//		ps.setString(1, likeDto.getMemberId());
+//		ps.setInt(2, likeDto.getProductNo());
+//		
+//		ps.execute();
+//		con.close();		
+//	}
+}
