e2fdee20ca1a7e865306197008ed12d7783eb792
Alwayshyuk <never5686@naver.com>
2022-05-03 15:14:47 +0900

    LikeDto

--- semi2/src/main/java/semi2/beans/LikeDao.java
+++ semi2/src/main/java/semi2/beans/LikeDao.java
@@ -2,9 +2,12 @@ package semi2.beans;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LikeDao {
-	public void insert(LikeDto likeDto) throws Exception{
+	public void insert(String memberId, int productNo) throws Exception{
 		
 		Connection con = JdbcUtils.getConnection();
 		
@@ -12,14 +15,48 @@ public class LikeDao {
 		
 		PreparedStatement ps = con.prepareStatement(sql);
 		
-		ps.setString(1, likeDto.getMemberId());
-		ps.setInt(2, likeDto.getProductNo());
+		ps.setString(1, memberId);
+		ps.setInt(2, productNo);
 		
 		ps.execute();
 		con.close();		
 	}
-	public boolean delete(LikeDto likeDto) throws Exception{
+	public boolean delete(String memberId, int productNo) throws Exception{
 		Connection con = JdbcUtils.getConnection();
-		String sql = "delete "
+		String sql = "delete like where member_id=? amd product_no=?";
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+		
+		ps.setString(1, memberId);
+		ps.setInt(2, productNo);
+		
+		int count = ps.executeUpdate();
+		
+		con.close();
+		
+		return count>0;
+	}
+	public List<LikeDto> selectAll(String memberId) throws Exception{
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "select * from like where memberId=?";
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+		
+		ps.setString(1, memberId);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		List<LikeDto> list = new ArrayList<>();
+		
+		while(rs.next()) {
+			LikeDto likeDto = new LikeDto();
+			likeDto.setMemberId(rs.getString("member_id"));
+			likeDto.setProductNo(rs.getInt("product_no"));
+			list.add(likeDto);
+		}
+		con.close();
+		
+		return list;
 	}
 }
