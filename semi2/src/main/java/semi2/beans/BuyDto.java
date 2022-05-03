77aae9e05a4c402ab97b42102f04014797e3a51b
Alwayshyuk <never5686@naver.com>
2022-05-03 12:14:45 +0900

    결제파트 Dto

--- semi2/src/main/java/semi2/beans/BuyDto.java
+++ semi2/src/main/java/semi2/beans/BuyDto.java
@@ -1,5 +1,41 @@
 package semi2.beans;
 
 public class BuyDto {
-
+	private int buyNo;
+	private String customerId;
+	private int orderNo;
+	private long buyDelivery;
+	private String buyStatus;
+	public int getBuyNo() {
+		return buyNo;
+	}
+	public void setBuyNo(int buyNo) {
+		this.buyNo = buyNo;
+	}
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	public int getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	public long getBuyDelivery() {
+		return buyDelivery;
+	}
+	public void setBuyDelivery(long buyDelivery) {
+		this.buyDelivery = buyDelivery;
+	}
+	public String getBuyStatus() {
+		return buyStatus;
+	}
+	public void setBuyStatus(String buyStatus) {
+		this.buyStatus = buyStatus;
+	}
+	
+	
 }
