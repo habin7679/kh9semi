9381d1011b2e8920cc8e15e280460040ac835eb6
JooDaEun <y0825131@naver.com>
2022-05-03 18:17:07 +0900

    memberDao 회원정보 수정 update()

--- semi2/src/main/java/semi2/beans/MemberDao.java
+++ semi2/src/main/java/semi2/beans/MemberDao.java
@@ -34,6 +34,31 @@ public class MemberDao {
 		
 		con.close();
 	}
+		//회원정보 수정
+		public boolean update(MemberDto memberDto) throws Exception {
+			Connection con = JdbcUtils.getConnection();
+			
+			String sql = "update member set member_pw = ?, member_name = ?, member_nick = ?, member_birth = ?, "
+					+ "member_email = ?, member_phone = ?, member_post = ?, member_basic_address = ?, member_detail_address = ? "
+					+ "where member_id = ?";
+			
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, memberDto.getMemberPw());
+			ps.setString(2, memberDto.getMemberName());
+			ps.setString(3, memberDto.getMemberNick());
+			ps.setString(4, memberDto.getMemberBirth());
+			ps.setString(5, memberDto.getMemberEmail());
+			ps.setString(6, memberDto.getMemberPhone());
+			ps.setString(7, memberDto.getMemberPost());
+			ps.setString(8, memberDto.getMemberBasicAddress());
+			ps.setString(9, memberDto.getMemberDetailAddress());
+			ps.setString(10, memberDto.getMemberId());
+			
+			int count = ps.executeUpdate();
+			con.close();
+			
+			return count > 0;
+		}
 		
 		//관리자 - 멤버 목록 전체조회
 		public List<MemberDto> selectList() throws Exception {
