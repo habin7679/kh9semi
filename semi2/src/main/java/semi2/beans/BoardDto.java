7ca63e3648680be495c0b15704b4f3536a42c579
syso950729 <syso950729@gmail.com>
2022-05-03 15:13:40 +0900

    boardDto

---
+++ semi2/src/main/java/semi2/beans/BoardDto.java
@@ -0,0 +1,34 @@
+package semi2.beans;
+
+import java.sql.Date;
+
+public class BoardDto {
+	private int boardNo;
+	private String boardHead;
+	private String boardTitle;
+	private String boardContent;
+	private Date boardTime;
+	private int boardReadcount;
+	private String boardWriter;
+	private int freeNo;
+	private int freeBoardNo;
+	private String freeWriter;
+	private int noticeNo;
+	private int noticeBoardNo;
+	private String noticeWriter;
+	private int tipNo;
+	private int tipBoardNo;
+	private String tipWriter;
+	private int csNo;
+	private int csBoardNo;
+	private int csProductNo;
+	private String csProductWriter;
+	private int reviewNo;
+	private int reviewBoardNo;
+	private int reviewProductNo;
+	private String reviewWriter;
+	private int replyNo;
+	private String replyContent;
+	private int reply_target;
+	
+}
