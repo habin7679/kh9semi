ea1f8eb319b70fdffd47ff7f1c89f16928b04fb6
chlqjatlr1 <eorlsha@naver.com>
2022-05-03 13:55:22 +0900

    회원 테이블

--- semi2/src/main/webapp/customer.sql
+++ semi2/src/main/webapp/customer.sql
@@ -1,18 +1,17 @@
-drop table customer;
-create table customer(
-customer_id varchar2(20) primary key check(regexp_like(member_id, '^[a-z][a-z0-9]{7,19}$')),
-customer_pw varchar2(16) not null 
+drop table member cascade CONSTRAINTS;
+create table member(
+member_id varchar2(20) primary key check(regexp_like(member_id, '^[a-z][a-z0-9]{7,19}$')),
+member_pw varchar2(16) not null 
 check(regexp_like(member_pw, '[a-z]+') and regexp_like(member_pw, '[A-Z]+')
 			and regexp_like(member_pw, '[0-9]+') and regexp_like(member_pw, '[!@#$]+')),
-customer_name varchar2(21) not null check(regexp_like(customer_name,'^[가-힣]{2,7}$')),
-customer_birth char(10) not null,
-customer_nick varchar2(30) check(regexp_like(customer_nick, '^[가-힣]{3,10}$')),
-customer_emai varchar2(100),
-customer_phone char(11) not null check(regexp_like(member_phone, '^010[1-9][0-9]{7}$')),
-customer_post varchar2(6),
-customer_basic_adress varchar2(300),
-customer_detail_adress varchar2(300) not null,
-customer_point number default 2000 not null check(member_point>=0),
-customer_grade varchar2(12) default '일반회원' not null check(member_grade in('일반회원','우수회원','vip','관리자')),
-customer_made_date date default sysdate not null
+member_nick varchar2(30) not null unique check(regexp_like(member_nick, '^[가-힣]{3,10}$')),
+member_phone char(11) not null check(regexp_like(member_phone, '^010[1-9][0-9]{7}$')),
+member_email varchar2(100),
+member_birth char(10) not null,
+member_post varchar2(6),
+member_basic_address varchar2(300),
+member_detail_address varchar2(300),
+member_point number default 20000 not null check(member_point >= 0),
+member_grade varchar2(12) default '일반회원' not null check(member_grade in ('일반회원', '우수회원', '관리자','vip')),
+member_joindate date default sysdate not null
 );
