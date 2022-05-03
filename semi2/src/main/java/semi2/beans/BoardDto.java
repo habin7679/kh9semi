28a394c7d60ea048c5a644ac40d8b0e4f6a047c0
syso950729 <syso950729@gmail.com>
2022-05-03 15:27:16 +0900

    boardDto

--- semi2/src/main/java/semi2/beans/BoardDto.java
+++ semi2/src/main/java/semi2/beans/BoardDto.java
@@ -29,6 +29,172 @@ public class BoardDto {
 	private String reviewWriter;
 	private int replyNo;
 	private String replyContent;
-	private int reply_target;
+	private int replyBoardNo;
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+	public String getBoardHead() {
+		return boardHead;
+	}
+	public void setBoardHead(String boardHead) {
+		this.boardHead = boardHead;
+	}
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+	public String getBoardContent() {
+		return boardContent;
+	}
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+	public Date getBoardTime() {
+		return boardTime;
+	}
+	public void setBoardTime(Date boardTime) {
+		this.boardTime = boardTime;
+	}
+	public int getBoardReadcount() {
+		return boardReadcount;
+	}
+	public void setBoardReadcount(int boardReadcount) {
+		this.boardReadcount = boardReadcount;
+	}
+	public String getBoardWriter() {
+		return boardWriter;
+	}
+	public void setBoardWriter(String boardWriter) {
+		this.boardWriter = boardWriter;
+	}
+	public int getFreeNo() {
+		return freeNo;
+	}
+	public void setFreeNo(int freeNo) {
+		this.freeNo = freeNo;
+	}
+	public int getFreeBoardNo() {
+		return freeBoardNo;
+	}
+	public void setFreeBoardNo(int freeBoardNo) {
+		this.freeBoardNo = freeBoardNo;
+	}
+	public String getFreeWriter() {
+		return freeWriter;
+	}
+	public void setFreeWriter(String freeWriter) {
+		this.freeWriter = freeWriter;
+	}
+	public int getNoticeNo() {
+		return noticeNo;
+	}
+	public void setNoticeNo(int noticeNo) {
+		this.noticeNo = noticeNo;
+	}
+	public int getNoticeBoardNo() {
+		return noticeBoardNo;
+	}
+	public void setNoticeBoardNo(int noticeBoardNo) {
+		this.noticeBoardNo = noticeBoardNo;
+	}
+	public String getNoticeWriter() {
+		return noticeWriter;
+	}
+	public void setNoticeWriter(String noticeWriter) {
+		this.noticeWriter = noticeWriter;
+	}
+	public int getTipNo() {
+		return tipNo;
+	}
+	public void setTipNo(int tipNo) {
+		this.tipNo = tipNo;
+	}
+	public int getTipBoardNo() {
+		return tipBoardNo;
+	}
+	public void setTipBoardNo(int tipBoardNo) {
+		this.tipBoardNo = tipBoardNo;
+	}
+	public String getTipWriter() {
+		return tipWriter;
+	}
+	public void setTipWriter(String tipWriter) {
+		this.tipWriter = tipWriter;
+	}
+	public int getCsNo() {
+		return csNo;
+	}
+	public void setCsNo(int csNo) {
+		this.csNo = csNo;
+	}
+	public int getCsBoardNo() {
+		return csBoardNo;
+	}
+	public void setCsBoardNo(int csBoardNo) {
+		this.csBoardNo = csBoardNo;
+	}
+	public int getCsProductNo() {
+		return csProductNo;
+	}
+	public void setCsProductNo(int csProductNo) {
+		this.csProductNo = csProductNo;
+	}
+	public String getCsProductWriter() {
+		return csProductWriter;
+	}
+	public void setCsProductWriter(String csProductWriter) {
+		this.csProductWriter = csProductWriter;
+	}
+	public int getReviewNo() {
+		return reviewNo;
+	}
+	public void setReviewNo(int reviewNo) {
+		this.reviewNo = reviewNo;
+	}
+	public int getReviewBoardNo() {
+		return reviewBoardNo;
+	}
+	public void setReviewBoardNo(int reviewBoardNo) {
+		this.reviewBoardNo = reviewBoardNo;
+	}
+	public int getReviewProductNo() {
+		return reviewProductNo;
+	}
+	public void setReviewProductNo(int reviewProductNo) {
+		this.reviewProductNo = reviewProductNo;
+	}
+	public String getReviewWriter() {
+		return reviewWriter;
+	}
+	public void setReviewWriter(String reviewWriter) {
+		this.reviewWriter = reviewWriter;
+	}
+	public int getReplyNo() {
+		return replyNo;
+	}
+	public void setReplyNo(int replyNo) {
+		this.replyNo = replyNo;
+	}
+	public String getReplyContent() {
+		return replyContent;
+	}
+	public void setReplyContent(String replyContent) {
+		this.replyContent = replyContent;
+	}
+	public int getReplyBoardNo() {
+		return replyBoardNo;
+	}
+	public void setReplyBoardNo(int replyBoardNo) {
+		this.replyBoardNo = replyBoardNo;
+	}
+	public BoardDto() {
+		super();
+	}
+	
 	
 }
