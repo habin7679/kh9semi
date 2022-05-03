753eb170547e538b77fd69a1d455d0b7486047d9
Alwayshyuk <never5686@naver.com>
2022-05-03 11:58:07 +0900

    Dto

---
+++ semi2/src/main/java/semi2/beans/cartDto.java
@@ -0,0 +1,27 @@
+package semi2.beans;
+
+public class cartDto {
+	private String customerId;
+	private int productNo;
+	private int cartAmount;
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	public int getProductNo() {
+		return productNo;
+	}
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+	public int getCartAmount() {
+		return cartAmount;
+	}
+	public void setCartAmount(int cartAmount) {
+		this.cartAmount = cartAmount;
+	}
+	
+	
+}
---
+++ semi2/src/main/java/semi2/beans/likeDto.java
@@ -0,0 +1,20 @@
+package semi2.beans;
+
+public class likeDto {
+	private String memberId;
+	private String productNo;
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getProductNo() {
+		return productNo;
+	}
+	public void setProductNo(String productNo) {
+		this.productNo = productNo;
+	}
+	
+	
+}
