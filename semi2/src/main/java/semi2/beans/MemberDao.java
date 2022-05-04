d8870b1a1de26d2a48aa60d23a71a642f0898abd
JooDaEun <y0825131@naver.com>
2022-05-04 10:06:44 +0900

    memberDao update 추가

--- semi2/src/main/java/semi2/beans/MemberDao.java
+++ semi2/src/main/java/semi2/beans/MemberDao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 public class MemberDao {
-
+	//회원가입
 	public void join(MemberDto memberDto) throws Exception {
 		Connection con = JdbcUtils.getConnection();
 		
@@ -29,4 +29,30 @@ public class MemberDao {
 		con.close();
 		
 	}
+	
+	//회원정보 수정
+			public boolean update(MemberDto memberDto) throws Exception {
+				Connection con = JdbcUtils.getConnection();
+				
+				String sql = "update member set member_pw = ?, member_name = ?, member_nick = ?, member_birth = ?, "
+						+ "member_email = ?, member_phone = ?, member_post = ?, member_basic_address = ?, member_detail_address = ? "
+						+ "where member_id = ?";
+				
+				PreparedStatement ps = con.prepareStatement(sql);
+				ps.setString(1, memberDto.getMemberPw());
+				ps.setString(2, memberDto.getMemberName());
+				ps.setString(3, memberDto.getMemberNick());
+				ps.setString(4, memberDto.getMemberBirth());
+				ps.setString(5, memberDto.getMemberEmail());
+				ps.setString(6, memberDto.getMemberPhone());
+				ps.setString(7, memberDto.getMemberPost());
+				ps.setString(8, memberDto.getMemberBasicAddress());
+				ps.setString(9, memberDto.getMemberDetailAddress());
+				ps.setString(10, memberDto.getMemberId());
+				
+				int count = ps.executeUpdate();
+				con.close();
+				
+				return count > 0;
+			}
 }
