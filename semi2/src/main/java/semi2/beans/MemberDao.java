06aeca9700db694c29f808b787d2bc665848c84b
JooDaEun <y0825131@naver.com>
2022-05-04 10:23:18 +0900

    memberDao 단일조회 추가

--- semi2/src/main/java/semi2/beans/MemberDao.java
+++ semi2/src/main/java/semi2/beans/MemberDao.java
@@ -119,4 +119,76 @@ public class MemberDao {
 			con.close();
 			return list;
 		}
+		
+		//관리자 - 회원 검색
+		public List<MemberDto> selectList(String type, String keyword) throws Exception {
+			Connection con = JdbcUtils.getConnection();
+			
+			String sql = "select * from member where instr(#1, ?) > 0 order by member_id asc";
+			sql = sql.replace("#1", type);
+			
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, keyword);
+			ResultSet rs = ps.executeQuery();
+			
+			List<MemberDto> list = new ArrayList<>();
+			
+			while(rs.next()) {
+				MemberDto memberDto = new MemberDto();
+				memberDto.setMemberId(rs.getString("member_id"));
+				memberDto.setMemberPw(rs.getString("member_pw"));
+				memberDto.setMemberName(rs.getString("member_name"));
+				memberDto.setMemberNick(rs.getString("member_nick"));
+				memberDto.setMemberBirth(rs.getString("member_birth"));
+				memberDto.setMemberEmail(rs.getString("member_email"));
+				memberDto.setMemberPhone(rs.getString("member_phone"));
+				memberDto.setMemberPost(rs.getString("member_post"));
+				memberDto.setMemberBasicAddress(rs.getString("member_basic_address"));
+				memberDto.setMemberDetailAddress(rs.getString("member_detail_address"));
+				memberDto.setMemberPoint(rs.getInt("member_point"));
+				memberDto.setMemberGrade(rs.getString("member_grade"));
+				memberDto.setMemberJoindate(rs.getDate("member_join_date"));
+				
+				list.add(memberDto);
+			}
+			con.close();
+			
+			return list;
+		}
+		
+		//관리자 - 회원 단일조회
+		public MemberDto selectOne(String memberId) throws Exception {
+			Connection con = JdbcUtils.getConnection();
+			
+			String sql = "select * from member where member_id = ?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, memberId);
+			ResultSet rs = ps.executeQuery();
+			
+			MemberDto memberDto;
+			if(rs.next()) {
+				memberDto = new MemberDto();
+				memberDto.setMemberId(rs.getString("member_id"));
+				memberDto.setMemberPw(rs.getString("member_pw"));
+				memberDto.setMemberName(rs.getString("member_name"));
+				memberDto.setMemberNick(rs.getString("member_nick"));
+				memberDto.setMemberBirth(rs.getString("member_birth"));
+				memberDto.setMemberEmail(rs.getString("member_email"));
+				memberDto.setMemberPhone(rs.getString("member_phone"));
+				memberDto.setMemberPost(rs.getString("member_post"));
+				memberDto.setMemberBasicAddress(rs.getString("member_basic_address"));
+				memberDto.setMemberDetailAddress(rs.getString("member_detail_address"));
+				memberDto.setMemberPoint(rs.getInt("member_point"));
+				memberDto.setMemberGrade(rs.getString("member_grade"));
+				memberDto.setMemberJoindate(rs.getDate("member_join_date"));	
+			}
+			else {
+				memberDto = null;
+			}
+			con.close();
+			return memberDto;
+		}
+		 
+		
+		
 }
