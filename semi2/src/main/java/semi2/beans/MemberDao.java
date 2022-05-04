3ae506d1d1db147a604cc3e4f523ac1a5d5a33ad
chlqjatlr1 <eorlsha@naver.com>
2022-05-04 11:57:27 +0900

    Merge remote-tracking branch 'origin/main' into beonsik_member2

--- semi2/src/main/java/semi2/beans/MemberDao.java
+++ semi2/src/main/java/semi2/beans/MemberDao.java
@@ -2,6 +2,7 @@ package semi2.beans;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class MemberDao {
 
@@ -27,5 +28,38 @@ public class MemberDao {
 		ps.execute();
 		
 		con.close();
+	}
+
+	public MemberDto selectOne(String memberId) throws Exception{
+		Connection con= JdbcUtils.getConnection();
+		
+		String sql="select*from member where member_id=?";
+		PreparedStatement ps= con.prepareStatement(sql);
+		ps.setString(1, memberId);
+		ResultSet rs= ps.executeQuery();
+		
+		MemberDto memberDto;
+		if(rs.next()) {
+			memberDto=new MemberDto();
+			
+			memberDto.setMemberId(rs.getString("member_id"));
+			memberDto.setMemberPw(rs.getString("member_pw"));
+			memberDto.setMemberName(rs.getString("member_name"));
+			memberDto.setMemberNick(rs.getString("member_nick"));
+			memberDto.setMemberPhone(rs.getString("member_phone"));
+			memberDto.setMemberEmail(rs.getString("member_email"));
+			memberDto.setMemberBirth(rs.getString("member_birth"));
+			memberDto.setMemberPost(rs.getString("member_post"));
+			memberDto.setMemberBasicAddress(rs.getString("member_basic_address"));
+			memberDto.setMemberDetailAddress(rs.getString("member_detail_address"));
+			memberDto.setMemberPoint(rs.getInt("member_point"));
+			memberDto.setMemberGrade(rs.getString("member_grade"));
+			memberDto.setMemberJoindate(rs.getDate("member_joindate"));
+		}
+		else {
+			memberDto=null;
+		}
+		con.close();
+		return memberDto;
 	}
 }
---
+++ semi2/src/main/webapp/member/login.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<%
+	String referer=request.getHeader("Referer");
+%>
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="login.ez" method="post">
+	<div>
+		<div>
+			<h1>로그인</h1>
+		</div>
+		<%if(referer !=null){%>
+			<input type="hidden" value="<%=referer %>">
+		<%}%>
+		
+		<div>
+			<label>아이디</label>
+			<input type="text" name="memberId" autocomplete="off" placeholder="아이디">
+		</div>
+		
+		<div>
+			<label>비밀번호</label>
+			<input type="text" name="memberPw" placeholder="비밀번호">
+		</div>
+		
+		<div>
+			<input type="submit" value="로그인">
+		</div>
+	</div>
+	<% if(request.getParameter("error") !=null){ %>
+	<div>
+		<sapn>로그인 정보가 일치하지 않습니다</sapn>
+	</div>
+	<%} %>
+	</form>
+	
+</body>
+</html>
