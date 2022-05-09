88e9b54f156d5f12e9699304abe4f70b1e222575
JooDaEun <y0825131@naver.com>
2022-05-09 11:06:37 +0900

    주문취소/주문삭제

---
+++ semi2/src/main/java/semi2/servlet/admin/BuyCancelServlet.java
@@ -0,0 +1,35 @@
+package semi2.servlet.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import semi2.beans.BuyDao;
+import semi2.beans.BuyDto;
+@WebServlet(urlPatterns = "/admin/buy_cancel.ez")
+public class BuyCancelServlet extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			int buyNo = Integer.parseInt(req.getParameter("buyNo"));
+			BuyDao buyDao = new BuyDao();
+			boolean success = buyDao.cancelInvoice(buyNo);
+			
+			if(success) {
+				resp.sendRedirect("buy_detail.jsp?buyNo="+buyNo);
+			}
+			else {
+				resp.sendRedirect("buy_cancel_fail.jsp");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	
+	}
+}
