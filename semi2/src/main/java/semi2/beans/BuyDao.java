cf871304d6d9940983caa174c866d1194438862f
Alwayshyuk <never5686@naver.com>
2022-05-09 09:29:50 +0900

    리뷰 관련

--- semi2/src/main/java/semi2/beans/BuyDao.java
+++ semi2/src/main/java/semi2/beans/BuyDao.java
@@ -78,4 +78,24 @@ public class BuyDao {
 		con.close();
 		return list;
 	}
+	public BuyDto selectOne(int buyNo) throws Exception{
+		Connection con = JdbcUtils.getConnection();
+		String sql = "select * from buy where buy_no=?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		
+		ps.setInt(1, buyNo);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		BuyDto bDto = new BuyDto();
+		if(rs.next()) {
+			bDto.setBuyNo(buyNo);
+			bDto.setMemberId(rs.getString("member_id"));
+			bDto.setOrderNo(rs.getInt("order_no"));
+			bDto.setBuyInvoice(rs.getInt("buy_invoice"));
+			bDto.setBuyStatus(rs.getString("buy_status"));
+		}
+		con.close();
+		return bDto;
+	}
 }
