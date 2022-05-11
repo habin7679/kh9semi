f42d0cf5d29a3da9dad494958c2f2c797950acdb
habin7679 <habin7679@naver.com>
2022-05-11 10:07:55 +0900

    삭제

--- semi2/src/main/java/semi2/servlet/product/ProductDeleteServlet.java
+++ semi2/src/main/java/semi2/servlet/product/ProductDeleteServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import semi2.beans.AttachmentDao;
 import semi2.beans.ProductAttachmentDao;
 import semi2.beans.ProductAttachmentDto;
 import semi2.beans.ProductDao;
@@ -19,17 +20,18 @@ public class ProductDeleteServlet extends HttpServlet {
 		try {
 			int no = Integer.parseInt(req.getParameter("product_no"));
 		//int no = 98;
-//		ProductAttachmentDao productattachmentDao = new ProductAttachmentDao();
-//		ProductAttachmentDto productattachmentDto = productattachmentDao.selectOne(no);
-//		int ano = productattachmentDto.getAttachmentNo();
+		ProductAttachmentDao productattachmentDao = new ProductAttachmentDao();
+		ProductAttachmentDto productattachmentDto = productattachmentDao.selectOne(no);
+		int ano = productattachmentDto.getAttachmentNo();
 		//int ano=10;
 		
 		ProductDao productDao = new ProductDao();
+		AttachmentDao attachmentDao = new AttachmentDao();
 		boolean result = productDao.delete(no);
-//		boolean result1 = productattachmentDao.delete(ano); 
+		boolean result1 = attachmentDao.delete(ano); 
 		
 		
-if(result) {
+if(result && result1) {
 			resp.sendRedirect("product_delete_success.jsp");
 		}
 		else {
